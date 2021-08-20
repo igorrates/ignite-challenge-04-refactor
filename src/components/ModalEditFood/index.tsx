c3965c1099f4a26c395938a1bef75b1c22bae104
Igor Rates <igor@corebridge.net>
2021-08-19 22:54:33 -0300

    converted modalEditFood to typescript

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,47 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { Food } from '../../models/Food';
+import { FormHandles, SubmitHandler } from '@unform/core';
+
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleUpdateFood: (food: Food) => void;
+  editingFood: Food;
+}
+
+const ModalEditFood = ({ isOpen, editingFood, setIsOpen, handleUpdateFood }: ModalEditFoodProps) => {
+
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit: SubmitHandler<Food> = async data => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalEditFood;
